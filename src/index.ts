c5a824cc811058c5bb22c759c0c53ade80a0bef6
Mohamed Zekry <mohamed.zekry@z2data.com>
2022-11-23 00:20:44 +0200

    apply prettier and tslint changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import fspromises from 'fs/promises'
-import fs from 'fs';
-import path, { parse } from 'path';
-import { stringify } from 'querystring';
+import fspromises from 'fs/promises';
+import path from 'path';
 import sharp from 'sharp';
-
 
 const app = express();
 const port = 3000;
@@ -12,35 +9,47 @@ const port = 3000;
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
-  });
+});
 
-  app.use(express.static('assets'));
+app.use(express.static('assets'));
 
+app.get('/api/imageresize', async (req, res) => {
+    const imagename: string =
+        req.query.imagename == undefined ? '' : req.query.imagename.toString();
+    const width: number =
+        req.query.width == undefined
+            ? 0
+            : isNaN(parseInt(req.query.width.toString()))
+            ? 0
+            : parseInt(req.query.width.toString());
+    const height: number =
+        req.query.height == undefined
+            ? 0
+            : isNaN(parseInt(req.query.height.toString()))
+            ? 0
+            : parseInt(req.query.height.toString());
 
-
-
-  app.get('/api/imageresize',async (req,res)=>{
-    let imagename:string = req.query.imagename == undefined?'': req.query.imagename.toString();
-    let width:number = req.query.width == undefined ? 0 : isNaN(parseInt(req.query.width.toString()))?0:parseInt(req.query.width.toString()) ;
-    let height:number = req.query.height== undefined? 0 : isNaN(parseInt(req.query.height.toString()))?0:parseInt(req.query.height.toString()) ;
-    const location = path.join(__dirname, '../assets/images_resized', `${imagename}_${width}_${height}.jpg`);
-    const IsFileExist = await fspromises.stat(`assets/images_resized/${imagename}_${width}_${height}.jpg`).catch(e => false)
-    if(IsFileExist){
-        res.sendFile(location);
-    }
-    else{
-        const isOriginalFileExist = await fspromises.stat(`assets/images/${imagename}.jpg`).catch(e => false)
-        if(isOriginalFileExist)
-        {
-            await sharp(`assets/images/${imagename}.jpg`).resize(width,height).toFile(`assets/images_resized/${imagename}_${width}_${height}.jpg`);
-            res.sendFile(location);
+    const imagesResizedLocation = path.join(
+        __dirname,
+        '../assets/images_resized',
+        `${imagename}_${width}_${height}.jpg`
+    );
+    const IsFileExist = await fspromises
+        .stat(imagesResizedLocation)
+        .catch(() => false);
+    if (IsFileExist) {
+        res.sendFile(imagesResizedLocation);
+    } else {
+        const isOriginalFileExist = await fspromises
+            .stat(`assets/images/${imagename}.jpg`)
+            .catch(() => false);
+        if (isOriginalFileExist) {
+            await sharp(`assets/images/${imagename}.jpg`)
+                .resize(width, height)
+                .toFile(imagesResizedLocation);
+            res.sendFile(imagesResizedLocation);
+        } else {
+            res.send(`<h3 style="color: red">File doesn't exist</h3>`);
         }
-        else{
-
-            res.send(`<h3 style="color: red">File doesn't exist</h3>`)
-        }
-    
-
     }
-   
-  });
+});
